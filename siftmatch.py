3fe28ad996478572c004ad22beaa2463da0b7592
neumason <cunruiwang@qq.com>
2017-02-25 00:22:11 +0800

    add siftmatch

--- siftmatch.py
+++ siftmatch.py
@@ -20,23 +20,26 @@ for f in range(0,len(pics),2):
     kp2, desc2 = detector.detectAndCompute(img2_gray, None)
     bf =cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
     matches = bf.match(desc1, desc2)
-    print type(matches[0])
+
+    print type(matches[0].queryIdx)
     print dir(matches[0])
 
     for m,n in enumerate(matches):
-        print m
-        print dir(n)
+        print m,n.queryIdx,n.trainIdx
+        img1 = cv2.circle(img1,(int(kp1[n.queryIdx].pt[0]),int(kp1[n.queryIdx].pt[1])),int(kp1[n.queryIdx].size*0.5),(0,0,155),1)
+        img2 = cv2.circle(img2, (int(kp2[n.trainIdx].pt[0]), int(kp2[n.trainIdx].pt[1])),int(kp2[n.trainIdx].size * 0.5), (55, 255, 155), 1)
+
 
 
     matches = sorted(matches, key=lambda x: x.distance)
+
     img3 =cv2.drawMatches(img1, kp1, img2,kp2, matches[:40],img2.copy(),flags=2)
 
-    plt.imshow(img3)
-    plt.show()
-    time.sleep(1)
-    plt.close()
-    # if cv2.waitKey(10) == 27:
-    #     break
-    # else:
-    #     plt.close()
-    #     continue
+    cv2.imshow('image', img3)
+    cv2.imwrite("../result/"+str(f)+"_"+str(f+1)+".jpg", img3, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
+    if cv2.waitKey(0) == 27:
+        break
+    else:
+        continue
+
+cv2.destroyAllWindows()
